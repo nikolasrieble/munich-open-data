6ca0d927e64bb6d640580f381bdad68ddc41decf
Nikolas Rieble <nikolas.rieble@tulip.co>
2023-01-14 17:04:41 +0100

    A map is working

--- assets/README.md
+++ assets/README.md
@@ -6,3 +6,6 @@ https://www.suche-postleitzahl.org/downloads
 Munich district to code mapping
 https://www.muenchen.de/en/leben/service/postal-codes-munich
 
+Potential data sources to investigate
+
+http://insideairbnb.com/get-the-data/
--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,26 +1,41 @@
+<script>
+	// @ts-nocheck
+
+	import { onMount } from 'svelte';
+	import { Map } from '@onsvisual/svelte-maps';
+	import maplibre from 'maplibre-gl';
+	let map;
+	let zoom;
+	let center = {};
+	onMount(() => {
+		map.addControl(new maplibre.NavigationControl(), 'top-left');
+	});
+</script>
+
 <svelte:head>
 	<title>About</title>
 	<meta name="description" content="About this app" />
 </svelte:head>
 
 <div class="text-column">
-	<h1>About this app</h1>
+	<h1>This should show a map below</h1>
 
-	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
-	</p>
-
-	<pre>npm create svelte@latest</pre>
-
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
-
-	<p>
-		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled!
-	</p>
+	<span>
+		<Map
+			id="map"
+			style="https://api.maptiler.com/maps/streets/style.json?key=wxZrbxq3dPx17ds8LO59"
+			location={{ lng: 16.62662018, lat: 49.2125578, zoom: 14 }}
+			bind:map
+			bind:zoom
+			bind:center
+		/>
+	</span>
 </div>
+
+<style>
+	span {
+		width: 100%;
+		height: 100vh;
+		position: relative;
+	}
+</style>
